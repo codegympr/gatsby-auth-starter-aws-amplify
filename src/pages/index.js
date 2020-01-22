b5d2c322226017f38799cca1163ceb9916595c68
EC2 Default User <ec2-user@ip-172-31-39-0.ec2.internal>
2020-01-22 19:54:23 +0000

    added select options

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,15 +14,32 @@ const options = [
 ];
 
 const IndexPage = () => {
-  const [input, setInput] = useState()
-  const [selectedOption, setSelectedOption] = useState()
-  const handleChange = (e)=>console.log(e)
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState("")
+  const [selectedOption, setSelectedOption] = useState("")
+  const handleInputChange = (e)=>{
+    setInput(e.value)
+    setSelectedOption(e.label)
+    return console.log("input", input, e)
+  }
+  const handleOutputChange = (e)=>{
+    setOutput(e.value)
+    setSelectedOption(e.label)
+    return console.log("output", output, e)
+  }
   return (
     <Layout>
       <h1>Test</h1>
       <Select
         value={selectedOption}
-        onChange={handleChange}
+        onChange={handleInputChange}
+        options={options}
+      />
+      <br/>
+      <h1>To</h1>
+      <Select
+        value={selectedOption}
+        onChange={handleOutputChange}
         options={options}
       />
     </Layout>
